39fa1074b1bc43bfbcea48b86a3f825d48e2990e
vrushabhnan1998 <112608182+vrushabhnan1998@users.noreply.github.com>
2024-04-24 00:32:33 +0530

    initial second

--- executive-dashboard/src/app/app.routes.ts
+++ executive-dashboard/src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '',   redirectTo: 'dashboard', pathMatch: 'full' }, 
     {
-        path:"dashboard",
-        component:DashboardComponent,
+        path: "dashboard",
+        component: DashboardComponent,
     },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     {
-        path:"**",
+        path: "**",
         redirectTo: 'dashboard'
     },
 ];
